7a1893705b701f37f052b09bf18934e52cd22e71
Minifuse <jani.raitinenn@gmail.com>
2025-10-21 09:32:31 +0300

    Tehtävät 2.1 ja 2.3

---
+++ CppOhjelmointiKurssi/TEHT2-1_KäyttäjäNimi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// Uudellennimeä mainiksi jos haluat buildaa
+int main2() {
+	
+	std::string name;
+	std::cout << "Laita nimesi: \n";
+	std::cin >> name;
+	std::cout << "Tere! " + name;
+
+	return 1;
+}
---
+++ CppOhjelmointiKurssi/TEHT2-3_KolmionAla.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+// Uudellennimeä mainiksi jos haluat buildaa
+
+double static CalculateTriangleSurface(double width, double height) {
+	return (width * height) / 2;
+}
+
+int main3() {
+
+	double inputWidth;
+	double inputHeight;
+
+	std::cout << "Laita Kolmion leveys metreissä: \n";
+	std::cin >> inputWidth;
+	std::cout << "Laita Kolmion korkeus metreissä: \n";
+	std::cin >> inputHeight;
+
+	std::string output;
+	output = std::to_string(CalculateTriangleSurface(inputWidth, inputHeight));
+	std::cout << output + "m^2 ";
+
+	return 1;
+}
+
